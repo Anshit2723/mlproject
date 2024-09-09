fdf2566a67050f80580ca15ff93cc78211b20917
Anshit2723 <chaturvedi.anshit@gmail.com>
2024-09-09 23:31:05 +0530

    model trainer

---
+++ src/components/data_transformation.py
@@ -0,0 +1,202 @@
+import os
+
+import sys
+from dataclasses import dataclass
+
+
+
+import numpy as np
+import pandas as pd
+
+from sklearn.compose import ColumnTransformer
+
+from sklearn.impute import SimpleImputer
+
+from sklearn.pipeline import Pipeline
+
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+
+from src.exception import CustomException
+
+from src.logger import logging
+import logging
+
+
+
+from src.utils import save_object
+
+
+
+
+
+
+@dataclass
+
+class DataTransformationConfig:
+
+    preprocessor_obj_file_path=os.path.join("artifacts","preprocessor.pkl")
+
+
+
+class DataTransformation:
+
+    def __init__(self):
+
+        self.data_transformation_config=DataTransformationConfig()
+
+
+    def get_data_transformer_object(Self):
+
+        try:
+
+            numerical_columns = ['reading score', 'writing score']
+
+            categorical_columns = ['gender', 'race/ethnicity', 'parental level of education', 'lunch', 'test preparation course']
+        
+
+            num_pipeline=Pipeline(
+
+                steps=[
+
+                    ("imputer",SimpleImputer(strategy="median")),
+
+                    ("scaler",StandardScaler())
+
+
+
+                ])
+
+
+            cat_pipeline=Pipeline(
+
+
+                steps=[
+
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+
+                    ("one_hot_encoder",OneHotEncoder(sparse=True)),
+
+                    ("scaler",StandardScaler(with_mean=False))
+                
+                
+
+                ]
+
+            )
+
+
+
+            logging.info("numerical column standard scaling is comleted")
+
+            logging.info("categorical column encoding is comleted")
+
+
+            preprocessor=ColumnTransformer(
+
+                [
+
+                    ("num_pipeline",num_pipeline,numerical_columns),
+
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
+
+                    ]
+
+            )
+
+            return preprocessor
+        
+
+
+        except Exception as e:
+
+            raise CustomException(e,sys)
+
+
+
+    def initiate_data_transformation(self,train_data_path,test_data_path):
+
+
+        try:
+
+           
+            train_df=pd.read_csv(train_data_path)
+            test_df=pd.read_csv(test_data_path)
+
+
+            logging.info("Read the train and test data done")
+
+            logging.info("obtaining preprocessing object")
+
+
+            preprocessing_obj=self.get_data_transformer_object()
+
+
+            target_column_name="math score"
+
+            numerical_columns = ['reading score', 'writing score']
+
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+
+            target_feature_train_df=train_df[target_column_name]
+
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+
+            target_feature_test_df=test_df[target_column_name]
+
+
+            logging.info("applying preprocessing object to training and testing dataframe")
+
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            
+
+            train_array = np.c_[
+
+                input_feature_train_arr,np.array(target_feature_train_df)
+
+            ]
+
+
+            test_array = np.c_[
+
+                input_feature_test_arr,np.array(target_feature_test_df)
+
+            ]
+
+
+            logging.info("Saved preprocessing object")
+
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+
+                obj=preprocessing_obj
+
+            )
+
+
+            return(
+
+                train_array,
+
+                test_array,
+
+                self.data_transformation_config.preprocessor_obj_file_path,
+
+
+
+
+            )
+       
+
+
+        except Exception as e:
+
+            raise CustomException(e,sys)
+            
--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,12 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_detail: sys):
+def error_message_detail(error_message,error_detail: sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Eroor occured in python script name [{0}] line number [{1}] error message[{2}].format()"
-    file_name,exc_tb.tb_lineno,str(error)
+    line_number = exc_tb.tb_lineno
+    error_message="Error occured in python script name {0} line number {1} error message{2}".format(file_name, line_number, str(error_message))
+    file_name,exc_tb.tb_lineno,str(error_message)
 
 
     return error_message
